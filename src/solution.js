a787af35beb204857a11f965fccb5077aee496b7
minnemullin <b@minbulat.ru>
2019-01-19 19:58:16 +0300

    First solution

--- src/solution.js
+++ src/solution.js
@@ -1,7 +1,64 @@
-const solution = function(graph, start, finish)  {
+const solution = (graph, start, finish) => {
+    const distances = {},
+        paths = {},
+        unvisited = [];
+    for (let node in graph) {
+        distances[node] = node === start ? 0 : Infinity;
+        paths[node] = [];
+        // if (node !== start)
+        unvisited.push(node)
+    }
+
+    while (unvisited.length > 0) {
+        const minNode = unvisited.sort((a, b) => {
+            distances[a] > distances[b] ? -1 : 1;
+        })[0];
+        // console.log(minNode);
+        // unvisited.indexOf()
+
+        unvisited.splice(unvisited.indexOf(minNode) , 1)
+        if (minNode === finish) {
+            console.log('finishhhh');
+
+            return {
+                distance: distances[finish],//90;
+                path: [finish, ...paths[finish], start].reverse() //['start', 'A', 'D', 'finish'];
+            }
+        } else {
+            for (let n in graph[minNode]) {
+                console.log(distances[n], distances[minNode] , graph[minNode][n]);
+                if (distances[n] > distances[minNode] + graph[minNode][n]) {
+                    distances[n] = distances[minNode] + graph[minNode][n]
+                    paths[n].push(minNode)
+                    console.log("+", distances[n], distances[minNode] , graph[minNode][n])
+                }
+            }
+        }
+        // for (let u in graph) {
+        //     U.push(u)
+        //     if (U.indexOf(u) > -1)
+        //         console.log(u, 'dfsdfsd');
+        //     d[u] = u === start ? 0 : Infinity;
+        //     p[u] = [];
+        // }
+        //
+    }
+
+    // return {
+    //     distance: distances[finish],//90;
+    //     path: paths[finish] //['start', 'A', 'D', 'finish'];
+    // }
     // Ваше решение
-	return {
-    distance: 90,
-    path: ['start', 'A', 'D', 'finish']
-}
-}
+};
+
+// const sol =
+//     solution({
+//             start: {A: 50, B: 20},
+//             A: {C: 40, D: 20},
+//             B: {A: 90, D: 90},
+//             C: {D: 160, finish: 50},
+//             D: {finish: 20},
+//             finish: {}
+//         }, 'start', 'finish',
+//     );
+// console.log(sol);
